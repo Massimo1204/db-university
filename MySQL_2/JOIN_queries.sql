eb5408c31b3cd438e59ebe8b5ae83684fbb3cb38
Massimo1204 <xiangmassimo@gmail.com>
2022-04-29 16:29:34 +0200

    es 1-2-3 JOIN queries

---
+++ MySQL_2/JOIN_queries.sql
@@ -0,0 +1,37 @@
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
+-- relativo dipartimento, in ordine alfabetico per cognome e nome
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
+-- superare ciascuno dei suoi esami
+
+-- 1
+SELECT `students`.`name` AS 'student_name',
+`students`.`surname` AS 'student_surname',
+`degrees`.`name` AS 'degree_name'
+FROM `students`
+JOIN `degrees` ON `degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
+-- 2
+SELECT `degrees`.`name` AS 'degree_name',
+`degrees`.`level` AS 'degree_level',
+`departments`.`name` AS 'department_name'
+FROM `degrees`
+JOIN `departments` ON `department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+-- 3
+SELECT `courses`.`name` AS 'course_name',
+`teachers`.`name` AS 'teacher_name',
+`teachers`.`surname` AS 'teacher_surname'
+FROM `teachers`
+JOIN `course_teacher` ON `teachers`.`id` = `teacher_id`
+JOIN `courses` ON `course_id` = `courses`.`id`
+WHERE `teachers`.`id` = 44;
+
+-- 4
+
