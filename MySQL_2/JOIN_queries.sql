dd87f6a7c0373ce192b4a1f05f9949db6a3a0198
Massimo1204 <xiangmassimo@gmail.com>
2022-04-29 17:12:56 +0200

    exersices 5-6 JOIN queries

--- MySQL_2/JOIN_queries.sql
+++ MySQL_2/JOIN_queries.sql
@@ -9,29 +9,48 @@
 -- superare ciascuno dei suoi esami
 
 -- 1
-SELECT `students`.`name` AS 'student_name',
-`students`.`surname` AS 'student_surname',
-`degrees`.`name` AS 'degree_name'
+SELECT `students`.`name` AS `student_name`,
+`students`.`surname` AS `student_surname`,
+`degrees`.`name` AS `degree_name`
 FROM `students`
 JOIN `degrees` ON `degree_id` = `degrees`.`id`
-WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+WHERE `degrees`.`name` = `Corso di Laurea in Economia`;
 
 -- 2
-SELECT `degrees`.`name` AS 'degree_name',
-`degrees`.`level` AS 'degree_level',
-`departments`.`name` AS 'department_name'
+SELECT `degrees`.`name` AS `degree_name`,
+`degrees`.`level` AS `degree_level`,
+`departments`.`name` AS `department_name`
 FROM `degrees`
 JOIN `departments` ON `department_id` = `departments`.`id`
-WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+WHERE `departments`.`name` = `Dipartimento di Neuroscienze`;
 
 -- 3
-SELECT `courses`.`name` AS 'course_name',
-`teachers`.`name` AS 'teacher_name',
-`teachers`.`surname` AS 'teacher_surname'
+SELECT `courses`.`name` AS `course_name`,
+`teachers`.`name` AS `teacher_name`,
+`teachers`.`surname` AS `teacher_surname`
 FROM `teachers`
 JOIN `course_teacher` ON `teachers`.`id` = `teacher_id`
 JOIN `courses` ON `course_id` = `courses`.`id`
 WHERE `teachers`.`id` = 44;
 
 -- 4
+SELECT `students`.`surname` AS `student_surname`,
+`students`.`name` AS `student_name`,
+`departments`.`name` AS `department_name`,
+`degrees`.`*`
+FROM `students`
+JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `student_surname` ASC;
+
+-- 5
+SELECT `degrees`.`name` AS `degree_name`,
+`courses`.`name` AS `course_name`,
+`teachers`.`surname` AS `teacher_surname`,
+`teachers`.`name` AS `teacher_name`
+FROM `degrees`
+JOIN `courses` ON `degrees`.`id` = `degree_id`
+JOIN `course_teacher` ON `courses`.`id` = `course_id`
+JOIN `teachers` ON `teacher_id` = `teachers`.`id`
+ORDER BY `degree_name`;
 
